9619e2860412220240afba121a70c6619ff103fb
Connor Rhone <syn3rt@gmail.com>
2018-08-10 12:42:49 +0100

    New weapon pad (does nothing yet), pads now pulse

---
+++ Assets/Scripts/MaterialPulse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialPulse : MonoBehaviour
+{
+    [SerializeField] private float min = 0.5f;
+    [SerializeField] private float max = 1.5f;
+    [SerializeField] private float speed = 1.0f;
+
+    private Material m_mat;
+    private Color m_col;
+
+    void Start()
+    {
+        m_mat = GetComponentInChildren<Renderer>().material;
+        m_col = m_mat.GetColor("_EmissionColor");
+    }
+	
+	void Update()
+    {
+        m_mat.SetColor("_EmissionColor", m_col * (min + Mathf.PingPong(Time.time * speed, (max - min))));
+    }
+}
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    [SerializeField] private float flatBoost = 25.0f;
+    [SerializeField] private float percentBoost = 25.0f;
+    [SerializeField] private bool isBooster = true;
+
+	void Start()
+    {
+
+	}
+	
+	void Update()
+    {
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name != "ShipModel") return;
+        ShipController ship = other.gameObject.GetComponentInParent<ShipController>();
+        if(ship)
+        {
+            if (isBooster) BoosterPad(ship);
+            else WeaponPad(ship);
+        }
+    }
+
+    void BoosterPad(ShipController ship)
+    {
+        ship.GetComponent<Rigidbody>().AddForce(transform.forward * (flatBoost + ship.GetMaxSpeed() * percentBoost), ForceMode.Impulse);
+    }
+
+    void WeaponPad(ShipController ship)
+    {
+
+    }
+}
