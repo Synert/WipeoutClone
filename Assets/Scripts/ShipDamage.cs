7a22f23915bd41b8ae223c378aae44eeb91f5b23
Connor Rhone <syn3rt@gmail.com>
2018-08-13 04:03:56 +0100

    Hitting walls now causes sparks to fly, slight tweaks to ship exhaust particles

---
+++ Assets/Scripts/ShipDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipDamage : MonoBehaviour
+{
+    [SerializeField] private GameObject sparks, smallSparks;
+
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            GameObject newSparks = Instantiate(sparks);
+            newSparks.transform.position = contact.point;
+            newSparks.transform.LookAt(transform);
+            //newSparks.transform.rotation = Quaternion.Euler(contact.normal);
+            newSparks.GetComponent<ParticleSystem>().startSpeed = 15.0f + collision.relativeVelocity.sqrMagnitude * 0.01f;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 10)
+        {
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                GameObject newSparks = Instantiate(smallSparks);
+                newSparks.transform.position = contact.point;
+                newSparks.transform.LookAt(transform);
+                //newSparks.transform.rotation = Quaternion.Euler(contact.normal);
+            }
+        }
+    }
+}
